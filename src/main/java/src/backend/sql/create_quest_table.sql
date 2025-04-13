b4c50a0e4bc576b876326956cec24c7ae26ca1d2
codinglyoko <codinglyoko@tutanota.com>
2025-04-13 18:57:54 -0400

    Increase Quest Description size in DB

--- src/main/java/src/backend/sql/create_quest_table.sql
+++ src/main/java/src/backend/sql/create_quest_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS quest;
 CREATE TABLE quest (
     id UUID PRIMARY KEY,
     quest_name VARCHAR(64),
-    quest_description VARCHAR(1024),
+    quest_description VARCHAR(8096),
     occurrence_type VARCHAR(64),
     due_date TIMESTAMP,
     experience_points INT
